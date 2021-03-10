a20bcc9525baca794ee9cc6377bc67ea029cce11
NotBioWaste905 <59259188+NotBioWaste905@users.noreply.github.com>
2021-03-11 00:32:47 +0300

    get all pages
    В файле лежат все пирожки с сайта

--- parser.py
+++ parser.py
@@ -1,22 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
-
+"""
 pages = []
 read = ' '
 while read != '':
     read = input("Enter URLs: ")
     pages.append(read)
-
+"""
 output_file_path = input("Path to output file: ")
 
 output_file = open(output_file_path, 'w', encoding="utf-8")
 
-for URL in pages:
+print("Loading started")
+
+for page in range(1, 1601):
+    URL = 'https://poetory.ru/pir/rating/'
+    URL += str(page)
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'lxml')
     quotes = soup.find_all('div', class_="item-text")
+
+    print("Page", str(page) + "...")
 
     for item in quotes:
         stikh = str(item)
         stikh = stikh.lstrip('<div class="item-text">').rstrip('</div>')
         output_file.write(stikh + '\n\n')
+    print(stikh)
+    print("Done")
