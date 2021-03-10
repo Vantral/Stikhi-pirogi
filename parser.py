6a0708626d70bf592315fca2cbf3d78126f3a24b
NotBioWaste905 <59259188+NotBioWaste905@users.noreply.github.com>
2021-03-10 14:01:12 +0300

    now works

--- parser.py
+++ parser.py
@@ -4,5 +4,9 @@ from bs4 import BeautifulSoup
 URL = "https://poetory.ru/pir/rating"
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('div', class_="Content")
-print(quotes)
+quotes = soup.find_all('div', class_="item-text")
+
+for item in quotes:
+    stikh = str(item)
+    stikh = stikh.lstrip('<div class="item-text">').rstrip('</div>')
+    print(stikh, end='\n\n')
