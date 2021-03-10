d6634650fd0572747873014909448d09c9b9dc6c
NotBioWaste905 <59259188+NotBioWaste905@users.noreply.github.com>
2021-03-10 14:54:30 +0300

    usable

--- parser.py
+++ parser.py
@@ -1,12 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://poetory.ru/pir/rating"
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('div', class_="item-text")
+pages = []
+read = ' '
+while read != '':
+    read = input("Enter URLs: ")
+    pages.append(read)
 
-for item in quotes:
-    stikh = str(item)
-    stikh = stikh.lstrip('<div class="item-text">').rstrip('</div>')
-    print(stikh, end='\n\n')
+output_file_path = input("Path to output file: ")
+
+output_file = open(output_file_path, 'w', encoding="utf-8")
+
+for URL in pages:
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('div', class_="item-text")
+
+    for item in quotes:
+        stikh = str(item)
+        stikh = stikh.lstrip('<div class="item-text">').rstrip('</div>')
+        output_file.write(stikh + '\n\n')
